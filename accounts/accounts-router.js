7b9574b6d43bbd83d249d8572c15291d698be212
Code4Blessings <robinwarden@code4blessings.com>
2020-01-23 13:22:42 -0500

    Delete endpoint tested successfuuly, PUT has a bug

--- accounts/accounts-router.js
+++ accounts/accounts-router.js
@@ -5,10 +5,8 @@ const router = express.Router();
 const dBase = require('../data/dbConfig')
 
 
-
-
 router.get('/', (req, res) => {
-    //select * from posts
+    //select * from accounts
     dBase.select('*')
     .from('accounts')
     .then(accounts => {
@@ -22,24 +20,89 @@ router.get('/', (req, res) => {
     })
 })
 
+//Get account by id
 router.get('/:id', (req, res) => {
-
+    const id = req.params.id
+    dBase('accounts'). where({
+        id: id
+    }).select('id')
+    .then(accountId => {
+        res.status(200).json(accountId)
+    })
+    .catch(err => {
+        res.status(500).json({
+            errorMessage: "Account ID cannot be retrieved",
+            message: err.message
+        })
+    })
 })
 
-router.get('/:id/comments', (req, res) => {
-
-})
-
+//Create An Account
 router.post('/', (req, res) => {
+    const data = {
+        name: req.body.name,
+        budget: req.body.budget
+    };
+    dBase('accounts').insert(data)
+        .then(postedAccount => {
+            dBase('accounts').where({id :postedAccount[0]}).first()
+                .then(newAccount => {
+                    res.status(201).json(newAccount);
+                })
+                .catch(err => {
+                    console.log(err);
+                    res.status(500).json({
+                        errorMessage: "Account could not be created",
+                        message: err.message
+                     });
+                });  
+        })
+        
+});
 
-})
-
+//Update An Account
 router.put('/:id', (req, res) => {
-
+    const id = req.params.id
+    const data = {
+        name: req.body.name,
+        budget: req.body.budget
+    };
+    dBase('accounts')
+        .where(id, data)
+        .update(data)
+        .then(accountEdited => {
+            res.status(201).json(accountEdited);
+        })  
+                .catch(err => {
+                    res.status(500).json({
+                        errorMessage: 'Unable to retrieve requested account',
+                        message: err.message
+                    });
+                });
 })
 
-router.delete('/:id', (req, res) => {
 
+//Delete Account
+router.delete('/:id', (req, res) => {
+    const id = req.params.id;
+    dBase('accounts')
+        .where({ id: id }).del()
+        .then(userRemoved => {
+            if (userRemoved) {
+                res.status(204).json(userRemoved)
+            }else {
+                res.status(404).json({
+                    errorMessage: "The account with the specified ID does not exist."
+        
+                })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                errorMessage: "The user could not be removed",
+                message: err.message
+            })
+        })
 })
 
 
